fbd2c8226cefb4e0f931363a7afa50f3c18b4a17
Damian Paul <damianpaul.93@gmail.com>
2015-11-17 11:59:34 +0100

    ApiInfoAdded

---
+++ WebApp/WebApp/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class InfoController : ApiController
+    {
+        // GET api/Employers
+        public string getInfo()
+        {
+            return "Info";
+        }
+    }
+}
