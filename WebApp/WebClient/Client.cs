e8348de46262c97459a455db7bb3a20b133d6974
Damian Paul <damianpaul.93@gmail.com>
2015-11-20 16:29:27 +0100

    ...

---
+++ WebApp/WebClient/Client.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient
+{
+    class Client
+    {
+        private RestClient client = new RestClient("http://localhost:64656/");
+        RestRequest request = new RestRequest("Employers", Method.GET);
+        IRestRequest<List<Employee>> response = client.Execute<List<Employee>>(request);
+    }
+}
